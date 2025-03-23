2b3e477dd80503cd32cdcceccfdc5956328f3f4b
Al Taaj <altaaj23@gmail.com>
2025-03-23 12:54:45 +0700

    feat: create register page

--- ewalled-web/src/pages/RegisterPage.jsx
+++ ewalled-web/src/pages/RegisterPage.jsx
@@ -1,7 +1,27 @@
+import registerBg from "../assets/loginregister.png";
+import logo from "../assets/logo-loginregister.png";
+import { Link } from "react-router-dom";
+import "../styles/RegisterPage.css";
+
 function RegisterPage() {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className="register-container">
+      <div className="register-left">
+        <img src={logo} alt="logo" className="logo" />
+        <form className="register-form">
+          <input type="text" placeholder="Nama Lengkap" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="text" placeholder="No HP" />
+          <button type="submit">Daftar</button>
+        </form>
+        <p className="login-text">
+          Sudah punya akun? <Link to="/login">Login di sini</Link>
+        </p>
+      </div>
+      <div className="register-right">
+        <img src={registerBg} alt="register-bg" />
+      </div>
     </div>
   );
 }
