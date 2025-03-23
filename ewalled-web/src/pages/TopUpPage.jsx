0c397f5c9c4e1d58939881d62ea3da7f8e795124
Al Taaj <altaaj23@gmail.com>
2025-03-23 17:37:45 +0700

    feat: create TopUpPage

--- ewalled-web/src/pages/TopUpPage.jsx
+++ ewalled-web/src/pages/TopUpPage.jsx
@@ -1,11 +1,28 @@
 import Nav from "../components/Nav";
+import "../styles/TopUpPage.css";
+import DropdownInput from "../components/DropdownInput";
+import AmountInput from "../components/AmountInput";
+import NoteInput from "../components/NoteInput";
+import PrimaryButton from "../components/PrimaryButton";
 
 function TopUpPage() {
   return (
     <>
       <Nav />
-      <div>
-        <h1>Top Up Page</h1>
+      <div className="topup-page">
+        <div className="topup-container">
+          <h1 className="topup-heading">Top Up</h1>
+          <div className="topup-card">
+            <AmountInput
+              label="Amount"
+              value="IDR 150.000,00"
+              balance="IDR 10.000.000"
+            />
+            <DropdownInput label="From" value="Credit Card" />
+            <NoteInput />
+            <PrimaryButton text="Top Up" />
+          </div>
+        </div>
       </div>
     </>
   );
