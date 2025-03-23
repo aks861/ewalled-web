79fce95b7468b0060221aed0c68c6b9e637d263b
Al Taaj <altaaj23@gmail.com>
2025-03-23 12:42:33 +0700

    feat: create login page

--- ewalled-web/src/pages/LoginPage.jsx
+++ ewalled-web/src/pages/LoginPage.jsx
@@ -1,7 +1,25 @@
+import loginBg from "../assets/loginregister.png";
+import logo from "../assets/logo-loginregister.png";
+import { Link } from "react-router-dom";
+import "../styles/LoginPage.css";
+
 function LoginPage() {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="login-container">
+      <div className="login-left">
+        <img src={logo} alt="logo" className="logo" />
+        <form className="login-form">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+        <p className="register-text">
+          Belum punya akun? <Link to="/register">Daftar di sini</Link>
+        </p>
+      </div>
+      <div className="login-right">
+        <img src={loginBg} alt="money" />
+      </div>
     </div>
   );
 }
